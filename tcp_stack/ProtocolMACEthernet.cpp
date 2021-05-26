185392a72ad2c0ccbb5cb0a944eedf3420e7de59
Robert Kimball <bobkimball@gmail.com>
2021-05-26 18:52:16 +0000

    Update license date

--- tcp_stack/ProtocolMACEthernet.cpp
+++ tcp_stack/ProtocolMACEthernet.cpp
@@ -1,5 +1,5 @@
 //----------------------------------------------------------------------------
-// Copyright(c) 2015-2020, Robert Kimball
+// Copyright(c) 2015-2021, Robert Kimball
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
